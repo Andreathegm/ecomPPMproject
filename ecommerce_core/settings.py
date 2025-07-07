3bddcac345d3d9bb5d09f343ffa0803362f23723
Andrea <innocentiandrea6@gmail.com>
2025-07-07 10:44:53 +0200

    prepare to deploy

--- ecommerce_core/settings.py
+++ ecommerce_core/settings.py
@@ -38,7 +38,7 @@ cloudinary.config(
 # DEBUG = False
 #
 # ALLOWED_HOSTS = []
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
